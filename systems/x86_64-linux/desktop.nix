ed913343293c709486f91006713ba748cc5860e8
Paul Walker <paulwalker@paulwalker.dev>
2023-10-14 20:28:28 +0000

    Add desktop

--- systems/x86_64-linux/desktop.nix
+++ systems/x86_64-linux/desktop.nix
@@ -1,1 +1,46 @@
-{ ... }: { }
+{ modules,config, lib, pkgs, modulesPath, ... }: {
+  imports =
+    [
+modules.common
+modules.gnome
+modules.tailscale
+modules.nvidia
+modules.home-manager
+ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
+  boot.loader.systemd-boot.enable = true;
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/299efe96-b767-4035-969d-895b922a0896";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/47C5-0110";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/5c8e9e7b-4603-4c88-9fdb-89afaa43407e"; }
+    ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
