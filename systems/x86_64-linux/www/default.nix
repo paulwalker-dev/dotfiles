855ac284f35735003b5dafccf21b87e7f6db829c
Paul Walker <paulwalker@paulwalker.dev>
2023-10-15 08:20:32 -0400

    test

---
+++ systems/x86_64-linux/www/default.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs,modules, modulesPath, ... }: {
+  imports = [
+    modules.common
+    modules.tailscale
+    modules.server
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
+
+  fileSystems."/" = {device = "/dev/disk/by-label/nixos"; fsType = "ext4"; };
+
+  swapDevices = [
+    {device = "/dev/disk/by-label/swap";}
+  ];
+
+  networking.useDHCP = lib.mkDefault true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
