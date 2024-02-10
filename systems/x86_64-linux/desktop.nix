9a9de107a6bccb19b79057543488b86749d7829c
Paul Walker <paulwalker@paulwalker.dev>
2024-02-10 16:33:43 -0500

    Add desktop

--- systems/x86_64-linux/desktop.nix
+++ systems/x86_64-linux/desktop.nix
@@ -1,8 +1,6 @@
 { modules, lib, modulesPath, config, ... }: {
   imports = [
     modules.common
-    modules.personal
-    modules.gnome
     modules.tailscale
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
@@ -11,12 +9,24 @@
   boot.loader.efi.efiSysMountPoint = "/boot";
   boot.loader.systemd-boot.enable = true;
 
-  # TODO Reinstall NixOS on laptop
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+  boot.kernelModules = [ "kvm-intel" ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/f321a176-6221-4eaa-bb1b-fe88836bced6";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/43C8-62BE";
+    fsType = "vfat";
+  };
 
   networking.useDHCP = lib.mkDefault true;
   networking.networkmanager.enable = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode =
+  hardware.cpu.intel.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
